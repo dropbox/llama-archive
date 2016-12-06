7349e1041a4294299edb01dd4a8ffa75dfe96cdf
Bryan Reed <vanilladuck@users.noreply.github.com>
2016-12-05 17:47:59 -0800

    Updating docs (#14)
    * Generating initial Sphinx config files for ReadTheDocs
    * Adding CHANGELOG and converting README to rst
    * Update conf.py
    * Fixing image URLs and switching to sphinx_rtd_theme for docs

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
@@ -126,6 +127,14 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
